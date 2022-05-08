67116576007889f4782aaeebed1734f578a1a5ca
Uriel Reyes <urielreyes514@gmail.com>
2022-05-07 19:16:37 -0500

    first commit

---
+++ resources/js/Pages/User.vue
@@ -0,0 +1,32 @@
+<template>
+    <AppLayout title="Users">
+        <template #header>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                Users
+            </h2>
+        </template>
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg">
+                    <pre>
+                        {{ users }}
+                   </pre
+                    >
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
+
+<script>
+import AppLayout from "@/Layouts/AppLayout.vue";
+
+export default {
+    props: ["users"],
+    components: {
+        AppLayout,
+    },
+};
+</script>
+
+<style></style>
